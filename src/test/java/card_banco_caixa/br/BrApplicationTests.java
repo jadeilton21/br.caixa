62da6922511458b699e8e8293ce7a2f38adcebf9
jadei <jadeiltondm@gmail.com>
2024-08-23 18:07:37 -0300

    feat

---
+++ src/test/java/card_banco_caixa/br/BrApplicationTests.java
@@ -0,0 +1,13 @@
+package card_banco_caixa.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
