c3cfe737f27720aee916fe48aae9e72775dfbccd
jadei <jadeiltondm@gmail.com>
2024-08-26 18:07:13 -0300

    feat: add anotation

--- src/main/java/card_banco_caixa/br/model/ItemDaBase.java
+++ src/main/java/card_banco_caixa/br/model/ItemDaBase.java
@@ -1,11 +1,16 @@
 package card_banco_caixa.br.model;
 
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class ItemDaBase {
 
-
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
 }
