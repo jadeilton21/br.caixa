7dd74607af542b5dbb73703096b6180a58cc38f7
jadei <jadeiltondm@gmail.com>
2024-08-26 18:17:53 -0300

    feat: add atributos

--- src/main/java/card_banco_caixa/br/model/Baner.java
+++ src/main/java/card_banco_caixa/br/model/Baner.java
@@ -3,6 +3,15 @@ package card_banco_caixa.br.model;
 
 import jakarta.persistence.Entity;
 
+import java.math.BigDecimal;
+
 @Entity(name = "tb_baner")
 public class Baner {
+
+
+    private Long id;
+
+    private String numeroDoBaner;
+
+    private BigDecimal limite;
 }
