b0a3edeb8d3980996a2d824805c2917ee5933cbe
jadei <jadeiltondm@gmail.com>
2024-08-26 18:19:06 -0300

    feat: add anotations

--- src/main/java/card_banco_caixa/br/model/Baner.java
+++ src/main/java/card_banco_caixa/br/model/Baner.java
@@ -1,17 +1,18 @@
 package card_banco_caixa.br.model;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
 @Entity(name = "tb_baner")
 public class Baner {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true)
     private String numeroDoBaner;
-
+    @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limite;
 }
