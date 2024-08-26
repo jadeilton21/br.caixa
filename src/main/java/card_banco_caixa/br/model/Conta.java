e8dd322890f20a739c7d820c8adf352b31c3980a
jadei <jadeiltondm@gmail.com>
2024-08-26 16:08:12 -0300

    feat: add atributos

--- src/main/java/card_banco_caixa/br/model/Conta.java
+++ src/main/java/card_banco_caixa/br/model/Conta.java
@@ -2,9 +2,28 @@ package card_banco_caixa.br.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
 
 @Entity(name = "tb_conta")
 public class Conta {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numero;
+
+    private String agencia;
+
+    private BigDecimal balance;
+
+    private BigDecimal limite;
+
 
 
 
