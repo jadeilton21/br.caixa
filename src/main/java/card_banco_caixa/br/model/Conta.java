fd62929838bb3a95adc1ba65fff7abf7fe5c5076
jadei <jadeiltondm@gmail.com>
2024-08-26 17:15:21 -0300

    feat: create get and set

--- src/main/java/card_banco_caixa/br/model/Conta.java
+++ src/main/java/card_banco_caixa/br/model/Conta.java
@@ -1,10 +1,7 @@
 package card_banco_caixa.br.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
@@ -15,17 +12,53 @@ public class Conta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true)
     private String numero;
 
     private String agencia;
-
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
-
+    @Column(name = "additional_limit", precision = 13, scale = 2)
     private BigDecimal limite;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getNumero() {
+        return numero;
+    }
 
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    public void setLimite(BigDecimal limite) {
+        this.limite = limite;
+    }
 }
