444329f7fe424d57189fad1b38fe603951a1116f
jadei <jadeiltondm@gmail.com>
2024-08-27 16:07:32 -0300

    feat: create anotations!!

--- src/main/java/card_banco_caixa/br/model/Usuario.java
+++ src/main/java/card_banco_caixa/br/model/Usuario.java
@@ -1,22 +1,30 @@
 package card_banco_caixa.br.model;
 
-import java.util.List;
+import jakarta.persistence.*;
 
+import java.util.List;
+@Entity(name = "tb_usuario")
 public class Usuario {
 
 
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String nome;
 
+    @OneToOne(cascade = CascadeType.ALL)
     private Conta conta;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 
     private List<Fatura> faturas;
-
+    @OneToOne(cascade = CascadeType.ALL)
     private Baner baner;
 
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Novos> novos;
+
 
 
 
