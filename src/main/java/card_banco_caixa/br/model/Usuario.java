b921d8f62274ee71784863c2d5e2da9cbddf4d90
jadei <jadeiltondm@gmail.com>
2024-08-27 15:26:52 -0300

    feat: create atributos

--- src/main/java/card_banco_caixa/br/model/Usuario.java
+++ src/main/java/card_banco_caixa/br/model/Usuario.java
@@ -1,4 +1,25 @@
 package card_banco_caixa.br.model;
 
+import java.util.List;
+
 public class Usuario {
+
+
+
+
+    private Long id;
+
+
+    private Conta conta;
+
+    private List<Fatura> faturas;
+
+    private Baner baner;
+
+
+    private List<Novos> novos;
+
+
+
+
 }
