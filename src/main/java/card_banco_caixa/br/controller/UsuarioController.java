8153f617662a77ab5826a76f03eb35945c042247
jadei <jadeiltondm@gmail.com>
2024-08-28 10:48:42 -0300

    feat: Create class UsuarioController

--- src/main/java/card_banco_caixa/br/controller/UsuarioController.java
+++ src/main/java/card_banco_caixa/br/controller/UsuarioController.java
@@ -1,6 +1,14 @@
 package card_banco_caixa.br.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    
 
 
 }
