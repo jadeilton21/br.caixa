bbf8079f5a7f4cb29fbd0ca35ea26f1a26f221c8
jadei <jadeiltondm@gmail.com>
2024-08-29 16:33:28 -0300

    feat: create crud encontrarUsurioPoId

--- src/main/java/card_banco_caixa/br/controller/UsuarioController.java
+++ src/main/java/card_banco_caixa/br/controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package card_banco_caixa.br.controller;
 
 
+import card_banco_caixa.br.doMain.model.Usuario;
 import card_banco_caixa.br.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,15 @@ public class UsuarioController {
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
+    }
+
+
+
+
+    @GetMapping("/{id}")
+    private ResponseEntity<Usuario> encontrarUsuarioPoId(@PathVariable Long id){
+        var user = usuarioService.encontrarUsuarioPoId(id);
+        return ResponseEntity.ok(user);
+
     }
 }
