969f0972f5bc2b0e9499d5229559678c33acbec7
jadei <jadeiltondm@gmail.com>
2024-08-29 16:34:54 -0300

    feat: create crud criarUsuario

--- src/main/java/card_banco_caixa/br/controller/UsuarioController.java
+++ src/main/java/card_banco_caixa/br/controller/UsuarioController.java
@@ -4,10 +4,10 @@ package card_banco_caixa.br.controller;
 import card_banco_caixa.br.doMain.model.Usuario;
 import card_banco_caixa.br.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -27,6 +27,17 @@ public class UsuarioController {
     private ResponseEntity<Usuario> encontrarUsuarioPoId(@PathVariable Long id){
         var user = usuarioService.encontrarUsuarioPoId(id);
         return ResponseEntity.ok(user);
+
+    }
+
+    @PostMapping
+    private ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario userToCreate){
+        var userCreated = usuarioService.criarUsuario(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
 
     }
 }
