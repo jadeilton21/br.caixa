1dfd0cf39969dd2eb3f721a742b303810dc05627
jadei <jadeiltondm@gmail.com>
2024-08-30 10:25:55 -0300

    feat: create method handleBusinessException

--- src/main/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobal.java
+++ src/main/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobal.java
@@ -3,6 +3,9 @@ package card_banco_caixa.br.controller.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -11,4 +14,9 @@ public class ManipuladorDeExcecaoGlobal {
 
     private final Logger logger = LoggerFactory.getLogger(ManipuladorDeExcecaoGlobal.class);
 
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 }
