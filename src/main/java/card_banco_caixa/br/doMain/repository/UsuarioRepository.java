7dc74312bb56a8c9f89141b61fc08e361146f38f
jadei <jadeiltondm@gmail.com>
2024-08-30 09:34:18 -0300

    feat: extends JpaRepository

--- src/main/java/card_banco_caixa/br/doMain/repository/UsuarioRepository.java
+++ src/main/java/card_banco_caixa/br/doMain/repository/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package card_banco_caixa.br.doMain.repository;
 
 
+import card_banco_caixa.br.doMain.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 
     Boolean existePorContaNumero(String contaNumero);
