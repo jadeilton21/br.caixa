d2f2ef79db451d3dbd8fd0ad6fca25ac930e8543
jadei <jadeiltondm@gmail.com>
2024-08-30 10:26:25 -0300

    feat: create method handleNotFoundException

--- src/main/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobal.java
+++ src/main/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobal.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class ManipuladorDeExcecaoGlobal {
 
@@ -18,5 +20,9 @@ public class ManipuladorDeExcecaoGlobal {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
         return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>("Resource ID not found.", HttpStatus.NOT_FOUND);
     }
 }
