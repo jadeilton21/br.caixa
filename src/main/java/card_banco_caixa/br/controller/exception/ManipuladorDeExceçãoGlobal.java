3b30c16df05398b4bcd61da4cc1fc5db8afc5165
jadei <jadeiltondm@gmail.com>
2024-08-30 10:24:44 -0300

    feat: class corrigida

--- src/main/java/card_banco_caixa/br/controller/exception/ManipuladorDeExceçãoGlobal.java
+++
@@ -1,8 +0,0 @@
-package card_banco_caixa.br.controller.exception;
-
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ManipuladorDeExceçãoGlobal {
-}
