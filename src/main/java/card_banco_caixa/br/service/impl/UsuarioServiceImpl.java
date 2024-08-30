0804c5cd37e3fe6cf72b5acfa5a373625f2c9c87
jadei <jadeiltondm@gmail.com>
2024-08-30 09:15:25 -0300

    feat: implements methods

--- src/main/java/card_banco_caixa/br/service/impl/UsuarioServiceImpl.java
+++ src/main/java/card_banco_caixa/br/service/impl/UsuarioServiceImpl.java
@@ -1,8 +1,23 @@
 package card_banco_caixa.br.service.impl;
 
+import card_banco_caixa.br.doMain.model.Usuario;
 import card_banco_caixa.br.service.UsuarioService;
 
 public class UsuarioServiceImpl implements UsuarioService {
 
 
+    @Override
+    public Usuario encontrarUsuarioPoId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Usuario criarUsuario(Usuario usuarioCriar) {
+        return null;
+    }
+
+    @Override
+    public Usuario alterarUsuario(Usuario usuarioAlterar) {
+        return null;
+    }
 }
