ac69b4e32227596fc9b8faff39ad8dcb7b4201dc
jadei <jadeiltondm@gmail.com>
2024-09-09 09:43:00 -0300

    feat: add anotations

--- src/test/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobalTest.java
+++ src/test/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobalTest.java
@@ -1,7 +1,13 @@
 package card_banco_caixa.br.controller.exception;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class ManipuladorDeExcecaoGlobalTest {
 
 }
