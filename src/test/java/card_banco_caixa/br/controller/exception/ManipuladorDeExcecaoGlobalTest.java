cfa2dab24ea6e9588b9b66725b15283ce155dc8f
jadei <jadeiltondm@gmail.com>
2024-09-09 09:59:27 -0300

    feat: create method test handleBusinessException

--- src/test/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobalTest.java
+++ src/test/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobalTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -20,5 +23,11 @@ class ManipuladorDeExcecaoGlobalTest {
     @Test
     @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas...")
     void handleBusinessException() throws Exception {
+
+        var response = mockMvc
+                .perform(post("/"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
