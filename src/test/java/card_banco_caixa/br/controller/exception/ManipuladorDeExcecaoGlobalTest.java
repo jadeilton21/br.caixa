63e22c1d2dd5a3063cf7a4e50bbc12b197610eff
jadei <jadeiltondm@gmail.com>
2024-09-09 09:44:03 -0300

    feat: atributo MockMvc

--- src/test/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobalTest.java
+++ src/test/java/card_banco_caixa/br/controller/exception/ManipuladorDeExcecaoGlobalTest.java
@@ -1,7 +1,9 @@
 package card_banco_caixa.br.controller.exception;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class ManipuladorDeExcecaoGlobalTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
