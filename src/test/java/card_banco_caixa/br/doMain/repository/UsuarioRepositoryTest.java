da2498fae9f52813cf0320d3312e3ed06cd3b6c6
jadei <jadeiltondm@gmail.com>
2024-09-10 13:52:17 -0300

    feat: add anotations and MockMvc

--- src/test/java/card_banco_caixa/br/doMain/repository/UsuarioRepositoryTest.java
+++ src/test/java/card_banco_caixa/br/doMain/repository/UsuarioRepositoryTest.java
@@ -1,7 +1,16 @@
 package card_banco_caixa.br.doMain.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
 class UsuarioRepositoryTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
 }
