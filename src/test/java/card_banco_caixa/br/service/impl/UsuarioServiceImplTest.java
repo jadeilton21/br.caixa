15529b8c530b43f83651da27f7691c0ede1771dc
jadei <jadeiltondm@gmail.com>
2024-09-10 16:10:24 -0300

    feat

--- src/test/java/card_banco_caixa/br/service/impl/UsuarioServiceImplTest.java
+++ src/test/java/card_banco_caixa/br/service/impl/UsuarioServiceImplTest.java
@@ -1,12 +1,17 @@
 package card_banco_caixa.br.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class UsuarioServiceImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
