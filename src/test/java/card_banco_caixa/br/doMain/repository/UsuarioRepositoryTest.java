95ac81f695b2a34459630730a3cdd7af2e52570f
jadei <jadeiltondm@gmail.com>
2024-09-10 14:01:15 -0300

    feat

--- src/test/java/card_banco_caixa/br/doMain/repository/UsuarioRepositoryTest.java
+++ src/test/java/card_banco_caixa/br/doMain/repository/UsuarioRepositoryTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -26,6 +28,10 @@ class UsuarioRepositoryTest {
 
         var response = mockMvc
                 .perform(post("/")).andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
 
 
     }
