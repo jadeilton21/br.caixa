0d4a014b3d1ad3f7bdd4d9bff620ce38f7ee20f2
jadei <jadeiltondm@gmail.com>
2024-09-10 16:15:20 -0300

    feat

--- src/test/java/card_banco_caixa/br/service/impl/UsuarioServiceImplTest.java
+++ src/test/java/card_banco_caixa/br/service/impl/UsuarioServiceImplTest.java
@@ -1,11 +1,16 @@
 package card_banco_caixa.br.service.impl;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -14,4 +19,15 @@ class UsuarioServiceImplTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    @DisplayName("Deve Devolver Código http 404 qundo informações forem invalidas..")
+    void encontrarUsuarioPoId()throws Exception {
+
+        var response =
+                mockMvc.perform(post("")).andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 }
