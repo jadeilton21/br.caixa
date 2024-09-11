4ecb387f4d9fb86e768e733b871c381d7ac6d6fd
jadei <jadeiltondm@gmail.com>
2024-09-11 09:15:40 -0300

    feat: test method encontrarUsuarioPoId

--- src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
+++ src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package card_banco_caixa.br.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,4 +14,8 @@ class UsuarioServiceTest {
     @Autowired
     private MockMvc mockMvc;
 
+
+    @Test
+    void encontrarUsuarioPoId() {
+    }
 }
