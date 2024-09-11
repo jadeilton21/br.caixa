85e56383879e6ee8e9ce503da677c1d9a245cba0
jadei <jadeiltondm@gmail.com>
2024-09-11 09:14:53 -0300

    feat: add biblioteca MockMvc

--- src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
+++ src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
@@ -1,11 +1,16 @@
 package card_banco_caixa.br.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class UsuarioServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
