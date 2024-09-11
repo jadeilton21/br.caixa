76db6331091f83400a4c886dcab962be5ac8bb50
jadei <jadeiltondm@gmail.com>
2024-09-11 09:27:14 -0300

    feat: test method encontrarUsuarioPoId

--- src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
+++ src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class UsuarioServiceTest {
@@ -20,6 +24,9 @@ class UsuarioServiceTest {
     @DisplayName("Deve Devolver Código http 404 qaundo informações forem invalidas..")
     void encontrarUsuarioPoId() throws Exception {
 
-        
+        var response = mockMvc.perform(post("")).andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
