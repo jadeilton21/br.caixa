5dc81bb1820ebd8d8b4a65686680ee1ab923e3fe
jadei <jadeiltondm@gmail.com>
2024-09-11 09:16:03 -0300

    feat: test method encontrarUsuarioPoId

--- src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
+++ src/test/java/card_banco_caixa/br/service/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package card_banco_caixa.br.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +17,9 @@ class UsuarioServiceTest {
 
 
     @Test
-    void encontrarUsuarioPoId() {
+    @DisplayName("Deve Devolver Código http 404 qaundo informações forem invalidas..")
+    void encontrarUsuarioPoId() throws Exception {
+
+        
     }
 }
